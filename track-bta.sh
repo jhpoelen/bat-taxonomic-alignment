2f0b24d9c74e52b6e66e5e6938ea1963fb24d421
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-05-21 16:57:53 -0500

    keep track of original and created alias for transformed tsv

--- track-bta.sh
+++ track-bta.sh
@@ -11,8 +11,36 @@
 
 set -xe
 
+#
+# version carriage return script 
+#
 echo\
- "curl https://zenodo.org/records/11193643/files/BTA_Version0.4_TSV\
- | tail -n+2\
+ tail -n+2\
  | tr '\r' ' ' "\
- | preston bash 
+ | preston track
+
+latest_version() {
+  preston ls -l tsv\
+  | grep hasVersion\
+  | cut -f3\
+  | head -1
+}
+
+SCRIPT_ID=latest_version
+
+#
+# version BTA 
+# 
+preston track https://zenodo.org/records/11193643/files/BTA_Version0.4_TSV
+
+BTA_ID=latest_version
+
+#
+# transform BTA version using script
+#
+preston cat ${BTA_ID}\
+ | preston bash -c "${SCRIPT_ID}"
+
+BTA_TSV_ID=latest_version
+
+preston alias "urn:example:bta.tsv" ${BTA_TSV_ID}
