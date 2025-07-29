54e098b549c318314c7fee633fae1d34978a565a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-29 08:51:47 -0500

    export bta v5 in csv/json/xslx/tsv using updated name headers

--- export-bta.sh
+++ export-bta.sh
@@ -6,8 +6,7 @@ exportTSV() {
  preston alias "urn:example:bta.tsv"\
  | head -n1\
  | preston cat\
- | tail -n+2\
- | sed -E 's/^name_MSW3\tname_HMW\tname_BatNames\tname_MDD\tname_IUCN\tname_BatNames_2023\tname_MDD_2023/Name_MSW3\tName_HMW\tName_BatNames\tName_MDD\tName_IUCN\tName_BatNames_2023\tName_MDD_2023/g'
+ | tail -n+2
 } 
 
 exportTSV\
