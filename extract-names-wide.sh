d53a67c6c5ddfd67bab1f68ad6e679fac81f71ff
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-05-14 20:07:34 -0500

    adjust to new headers

--- extract-names-wide.sh
+++ extract-names-wide.sh
@@ -11,11 +11,11 @@ COMPLETE_TSV_HASH=$(preston ls -l tsv | grep hasVersion | grep "urn:example:bta.
 
 preston cat ${COMPLETE_TSV_HASH}\
  | nl\
- | tail -n+2\
- | cut -f1-15\
+ | tail -n+1\
+ | cut -f1,4-18\
  | sed -E "s/^\s+//g"\
- | sed -E "s/^2\t/treatmentId\t/g"\
- | sed -E "s+^([0-9]*)+https://linker.bio/line:${COMPLETE_TSV_HASH}!/L2,L\1.tsv+g"\
+ | sed -E "s/^1\t/treatmentId\t/g"\
+ | sed -E "s+^([0-9]*)+https://linker.bio/line:${COMPLETE_TSV_HASH}!/L1,L\1.tsv+g"\
  | sed "s+.*treatmentId+treatmentId+g"\
  | mlr --tsvlite rename -r '(.*)([0-9]+)([.])([0-9]+)(.*),\1\2_\4\5'\
  | mlr --tsvlite rename -r 'name_(.*)_([0-9]{4})(.*),\2_\1\3'
