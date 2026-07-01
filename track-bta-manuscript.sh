0a73af09aeb3113d601e8cbe0d09d33612f63d60
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-07-01 11:12:13 -0500

    track bta manuscript

---
+++ track-bta-manuscript.sh
@@ -0,0 +1,5 @@
+# tracking 2026-07-01 working copy of BTA manuscript
+#
+#
+
+preston track "https://docs.google.com/document/d/1It1vERlYmt9ShPwHvHZQjKUG7XZXN9BKOSYDnZLfy3E/edit?tab=t.0" "https://docs.google.com/document/d/1It1vERlYmt9ShPwHvHZQjKUG7XZXN9BKOSYDnZLfy3E/edit?tab=t.mfl7fi6ao916"
